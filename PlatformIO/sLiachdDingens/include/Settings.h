353554652da995322dc75cfac55ffcd0e9871746
V0giK <119131833+V0giK@users.noreply.github.com>
2025-05-12 20:57:11 +0200

    Update Settings.h

--- PlatformIO/sLiachdDingens/include/Settings.h
+++ PlatformIO/sLiachdDingens/include/Settings.h
@@ -1,4 +1,17 @@
 // Settings.h
+/*
+  Projekt: sLiachdDingens-ESP8266
+  Copyright © 2025 V0giK
+
+  Dieses Programm ist freie Software: Du kannst es unter den Bedingungen
+  der GNU General Public License, wie von der Free Software Foundation
+  veröffentlicht, weitergeben und/oder modifizieren – entweder gemäß
+  Version 3 der Lizenz oder (nach Deiner Wahl) einer späteren Version.
+
+  Der vollständige Lizenztext ist in der Datei LICENSE enthalten:
+  https://github.com/V0giK/sLiachdDingens-ESP8266/blob/main/LICENSE
+*/
+
 #pragma once
 
 /*******************************************************************************************************************************************************************/
